dfc55f98745d891fdb83fae919ea0a8ac6f27085
Shubhrang-Sinha <sinhashubhrang2195@gmail.com>
2025-08-27 10:44:58 +0530

    commit

---
+++ assignment - 3/add_1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void Smaller(int arr[], int n) {
+    stack<int> st;
+
+    for (int i = 0; i < n; i++) {
+
+        while (!st.empty() && st.top() >= arr[i]) {
+
+            st.pop();
+        }
+
+        if (st.empty()) {
+            cout << -1 << " ";
+        } else {
+            cout << st.top() << " ";
+        }
+
+        st.push(arr[i]);
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        
+        cin >> arr[i];
+    }
+
+    cout << "Nearest smaller elements: ";
+    Smaller(arr, n);
+
+    return 0;
+}
